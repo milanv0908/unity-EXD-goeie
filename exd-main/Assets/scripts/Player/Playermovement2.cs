8de5a6273a9fb3b7a54d7bde5bf9bdb57440a408
milanv0908 <milanverduin2005@gmail.com>
2024-06-20 14:49:48 +0200

    xdsfsgh

--- exd-main/Assets/scripts/Player/Playermovement2.cs
+++ exd-main/Assets/scripts/Player/Playermovement2.cs
@@ -71,7 +71,7 @@ public class Playermovement2 : MonoBehaviour
 
         try
         {
-            sp = new SerialPort("COM3", 9600);
+            sp = new SerialPort("COM7", 9600);
             sp.Open();
             sp.ReadTimeout = 100; // Adjusting the read timeout to 100ms
             Debug.Log("Serial port opened successfully.");
